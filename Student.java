e2405850b95c0e084661738f4b21172d40685c09
Monica1901 <156048762+Monica1901@users.noreply.github.com>
2024-01-11 15:00:25 -0500

    Updated Code

--- Student.java
+++ Student.java
@@ -1,8 +1,13 @@
 
 public class Student {
-	String name = "Monica";
-	public String getName(){
+	
+	public static String getName(){
+		String name = "Monica";	
 		return name;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Student name is "+ Student.getName());
 	}
 
 }
